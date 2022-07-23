13603416010853050c2b92ad98c7225e5a02e2c9
Chiranjeevi Tapal <chiranjeevi.tapal@outlook.com>
2022-07-23 21:03:25 +0530

    refactored

--- app/server.js
+++ app/server.js
@@ -2,18 +2,18 @@ const express = require('express');
 const app = express();
 const moment = require("moment");
 const io = require("socket.io")({
-  allowEIO3: true // false by default
+    allowEIO3: true // false by default
 });
 
-app.set('port', process.env.PORT || 3000 );
+app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', 'app/views');
 
 app.use(express.static('app/public'));
 app.use(express.static('node_modules'));
-app.use((req, res, next)=>{
-  res.locals.moment = moment;
-  next();
+app.use((req, res, next) => {
+    res.locals.moment = moment;
+    next();
 });
 
 // ROUTES
@@ -23,13 +23,13 @@ app.use(require('./routes/api/api-controller'));
 app.use(require('./routes/chat/chat-controller'));
 
 let server = app.listen(app.get('port'), () => {
-  console.log('Listening on port ' + app.get('port'));
+    console.log('Listening on port ' + app.get('port'));
 });
 
 io.attach(server);
 io.on('connection', (socket) => {
-  socket.on('postMessage', (data) => {
-    io.emit('updateMessages', data);
-  });
+    socket.on('postMessage', (data) => {
+        io.emit('updateMessages', data);
+    });
 });
 
